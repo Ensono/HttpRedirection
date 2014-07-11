07af1041b8a7bb537ea5976f89cbff7e1b443041
Richard Slater <richard.slater@amido.com>
2014-07-11 07:08:37 +0100

    Added Alias:
      trhr -> Trace-HttpRedirect

--- HttpRedirection.psm1
+++ HttpRedirection.psm1
@@ -1,7 +1,17 @@
-# Dynamically populate the module ignore any Pester test fixtures.
+# Dynamically populate the module, ignore any Pester test fixtures.
 $functions = Get-ChildItem -Recurse "$PSScriptRoot\cmdlets" -Include *.ps1 | Where-Object { $_ -notmatch ".Tests.ps1" }
 
 # dot source the individual scripts that make-up this module
 foreach ($function in $functions) { . $function.FullName }
 
+# Dynamically populate the aliases, ignore any Pester test fixtures.
+$aliases = Get-ChildItem -Recurse "$PSScriptRoot\aliases" -Include *.ps1 | Where-Object { $_ -notmatch ".Tests.ps1" }
+
+# dot source the individual aliases that make-up this module
+foreach ($alias in $aliases) { . $alias.FullName }
+
+# export the module members
+Export-ModuleMember -Function * -Alias *
+
+# Win!
 Write-Host -ForegroundColor Green "Module $(Split-Path $PSScriptRoot -Leaf) was successfully loaded."
