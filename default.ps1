580eed1bf38a884de44c4cf9672b8dbd8467425a
Richard Slater <richard.slater@amido.com>
2015-02-13 22:53:16 +0000

    More Robust Process

--- default.ps1
+++ default.ps1
@@ -1,17 +1,21 @@
 Task default -Depends Test
 
-Task Test {
-  Get-Module -Name Pester | Remove-Module;
-  Import-Module ".\build\modules\Pester\Pester.psd1";
-  Invoke-Pester -ExcludeTag 'Integration' ".\src\";
+Task BuildOutputDirectory -PreCondition { -Not (Test-Path '.\build\output') } {
+  New-Item -Path '.\build\output' -Type Container;
 }
 
-Task Package -Depends Test{
+Task Test -Depends BuildOutputDirectory {  
+  Get-Module -Name Pester | Remove-Module;
+  Import-Module '.\build\modules\Pester\Pester.psd1';
+  Invoke-Pester -OutputFile 'build/output/Test-Results.xml' -OutputFormat NUnitXml -ExcludeTag 'Integration' ".\src\";
+}
+
+Task Package -Depends Test, BuildOutputDirectory {
   $SourceDirectory = Resolve-Path '.\src\';
-  $PackageFile = "{0}/HttpRedirection.zip" -f $PSScriptRoot;
+  $PackageFile = '{0}/build/output/HttpRedirection.zip' -f $PSScriptRoot;
   if (Test-Path $PackageFile) {
     Remove-Item $PackageFile;
   }
-  Add-Type -Assembly "System.IO.Compression.FileSystem";
+  Add-Type -Assembly 'System.IO.Compression.FileSystem';
   [System.IO.Compression.ZipFile]::CreateFromDirectory([string]$SourceDirectory, $PackageFile);
 }
